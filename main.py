774ca04acbb0e3f4cbc8cc3142c0a938805897c5
plikin <e-o-2022@yandex.ru>
2021-12-31 18:39:00 +0700

    commit fixed

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ class Student:
         res = f'Имя:  {self.name} Фамилия:  {self.surname} Средняя оценка за домашнее задание:  {self.grades} ' \
               f'Завершённые курсы:  {self.finished_courses}'
         return  res
+
+    def __lt__(self):
+        for student in Student:
+            if self < student:
+                return student
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -32,10 +38,14 @@ class Lecturer(Mentor):
         self.name = name
         self.surname = surname
         self.lec_grades = lec_grades
+
     def __str__(self):
         res = f'Имя:  {self.name} Фамилия:  {self.surname} Средняя оценка за лекцию:  {self.lec_grades}'
         return res
-
+    def __lt__(self):
+        for lecturer in Lecturer:
+            if self < lecturer:
+                return lecturer
 class Reviewer(Mentor):
    def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -45,12 +55,17 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-   def__str__(self):
+
+
+   def __str__(self):
         res = f'Имя: {self.name} Фамилия: {self.surname}'
         return res
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+
+
+
 
 
 #cool_mentor = Mentor('Some', 'Buddy')
@@ -67,7 +82,16 @@ best_lecturer = Lecturer('Isaak', 'Newton', 9.9)
 some_student = Student('Ruony', 'Eman', 9.6)
 best_student = Student('Wiliam', 'Gates', 9.8)
 
+def middle_value_hm(student, course):
+    if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        for student in Student:
+            
+
+    else:
+            return 'Ошибка'
 print(some_reviewer)
 print(some_lecturer)
 print(some_student)
 print(best_student.grades)
+
+middle_value_hm(Student, [])
